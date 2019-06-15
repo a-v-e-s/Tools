af1fec2206048152ace990b012b0a9332309dfaf
rock64 <rock64@rock64>
2019-06-15 09:57:47 -0500

    improved startup.py

--- startup.py
+++ startup.py
@@ -5,11 +5,14 @@ if os.getuid() != 0:
 	sys.exit()
 
 os.popen('sudo noip2')
-background = os.path.join('/home/rock64/Pictures/backgrounds', random.choice(os.listdir('/home/rock64/Pictures/backgrounds')))
-command = 'feh --bg-fill ' + background
-os.popen(command)
+os.popen('sudo mount -t ext4 /dev/mmcblk1p1 /srv/ftp')
 
 if input('\nStart ssh server? [y/n]\n').lower() == 'y':
 	os.popen('sudo systemctl start ssh')
+
+if '/usr/lib/xorg/Xorg' in subprocess.getoutput('ps -aux | grep Xorg'):
+	background = os.path.join('/home/rock64/Pictures/backgrounds', random.choice(os.listdir('/home/rock64/Pictures/backgrounds')))
+	command = 'feh --bg-fill ' + background
+	os.popen(command)
 	
 print('Done!')
