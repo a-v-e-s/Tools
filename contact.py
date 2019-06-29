97fe4e62458a1269a00bd00dee7e0ea65fefce62
rock64 <rock64@rock64>
2019-06-29 03:19:59 -0500

    ?

--- contact.py
+++ contact.py
@@ -8,7 +8,7 @@ name = form_data.getvalue('name')
 contact_info = form_data.getvalue('contact')
 message = form_data.getvalue('message')
 
-with open('latest.txt', 'w') as info:
+with open('../latest.txt', 'w') as info:
     info.write(name + '\n')
     info.write(contact_info + '\n')
     info.write(message + '\n')
