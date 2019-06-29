916fb83a3bcd48d4aa4108ac9a44a1e0fdc11b5e
Jon David Tannehill <jdtannehill2490@gmail.com>
2019-06-29 03:58:53 -0500

    altered rsync.sh

--- rsync.sh
+++ rsync.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # "sudo systemctl start nfs-kernel-server"  or start nfs-server with startup.py on rock64 first!
-sudo mount -t nfs 192.168.1.253:/srv /mnt/nfs
+# then sudo mount -t nfs 192.168.1.xxx:/srv /mnt/nfs
 cd /home/crowbar
 sudo rsync -aAv --delete --exclude=.cache /home/crowbar /media/crowbar/'Seagate Backup Plus Drive'/backupDocs/rsync
 sudo rsync -aAv --delete /mnt/nfs /media/crowbar/'Seagate Backup Plus Drive'/backupDocs/rsync/rock64
