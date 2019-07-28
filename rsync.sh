8fb9591d4537ac6c9895bcdf84c349ea77890844
Jon David Tannehill <jdtannehill2490@gmail.com>
2019-07-28 02:02:02 -0500

    updated rsync.sh

--- rsync.sh
+++ rsync.sh
@@ -2,8 +2,10 @@
 
 # "sudo systemctl start nfs-kernel-server"  or start nfs-server with startup.py on rock64 first!
 # then sudo mount -t nfs 192.168.1.xxx: /mnt/nfs
+# sudo mount /dev/sda? /mnt/nfs
 # if you configure your server & router to give the same ip to the server, you can uncomment the previous line
 cd /home/crowbar
-sudo rsync -aAv --delete --exclude=.cache /home/crowbar /media/crowbar/'Seagate Backup Plus Drive'/backupDocs/rsync
-sudo rsync -aAv --delete /mnt/nfs /media/crowbar/'Seagate Backup Plus Drive'/backupDocs/rsync/rock2
+sudo rsync -aAv --delete --exclude=.cache /home/crowbar /media/crowbar/'Seagate Backup Plus Drive'/backupDocs/rsync/debi
+sudo rsync -aAv --delete /mnt/nfs /media/crowbar/'Seagate Backup Plus Drive'/backupDocs/rsync/rock64
+sudo rsync -aAv --delete /mnt/hdd/Users/jdtan /media/crowbar/'Seagate Backup Plus Drive'/backupDocs/rsync/Windows
 echo Done!
