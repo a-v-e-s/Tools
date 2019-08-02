5ea8278b1828dac34a31fa1af7dab6811caa7437
rock64 <rock64@rock64>
2019-08-02 09:00:15 -0500

    flyswatter.py now blocks attackers trying to brute force non-existent php scripts from apache2 hosted website.

--- flyswatter.py
+++ flyswatter.py
@@ -76,13 +76,57 @@ def ip6(n):
 		return True
 	except socket.error:
 		return False
+		
+		
+def apache(log):
+	global ip_attackers
+	rules = open('/etc/iptables/rules.v4', 'r')
+	for x in rules:
+		if re.search('DROP', x):
+			for y in x.split():
+				if re.search('/32', y):			# this needs to be made more widely applicable
+					ip_attackers.append(y[:-3])	
+					
+	failed_attempts = []
+	for x in log:
+		if re.search('.*(\.php).*(\s404\s)', x):
+			failed_attempts.append(x)
+			
+	suspects = []
+	for x in failed_attempts:
+		possible_ip = x.split()[0]
+		if ip4(possible_ip):
+			suspects.append(possible_ip)
+		elif ip6(possible_ip):
+			suspects.append(possible_ip)
+		else:
+			for y in x.split()[1:]:
+				if ip4(y):
+					suspects.append(y)
+				elif ip6(y):
+					suspects.append(y)
 
+	new_attackers = [ip for ip, count in collections.Counter(suspects).items() if count > 12]
+	for x in new_attackers:
+		if x in ip_attackers:
+			new_attackers.remove(x)
+			
+	print('Blocking attackers now...')
+	for x in new_attackers:
+		command = 'iptables -I INPUT -s ' + x + ' -j DROP'
+		os.popen(command)
+		print(x, 'was blocked from gaining access to your computer!')
+	
 
 if len(sys.argv) == 1:
 	with open('/var/log/auth.log', 'r', encoding='utf8') as log:
 		main(log)
 	with open('/var/log/auth.log.1', 'r', encoding='utf8') as log:
 		main(log)
+	with open('/var/log/apache2/access.log', 'r', encoding='utf8') as log:
+		apache(log)
+	with open('/var/log/apache2/access.log.1', 'r', encoding='utf8') as log:
+		apache(log)
 elif len(sys.argv) > 1:
 	for x in sys.argv[1:]:
 		try:
