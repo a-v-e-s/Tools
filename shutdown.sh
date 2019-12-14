54ebe74a3ea4ee388c5b48dddc860835b872dff5
Jon David Tannehill <jdtannehill2490@gmail.com>
2019-12-13 22:03:29 -0600

    created shutdown.sh

---
+++ shutdown.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]; then
+	echo This script must be run as root!
+	exit
+fi
+
+rm -r --interactive=never /home/crowbar/.cache/thumbnails
+echo '' > /home/crowbar/.bash_history
+echo '' > /home/crowbar/.sqlite_history
+echo '' > /home/crowbar/.python_history
+
+shutdown -P now
