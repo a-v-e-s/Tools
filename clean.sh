28dfa905ce25494563696d2f050ec852fa970c9a
Jon David Tannehill <jdtannehill2490@gmail.com>
2019-12-22 13:53:02 -0600

    created clean.sh in place of shutdown.sh

---
+++ clean.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+rm -r --interactive=never /home/crowbar/.cache/thumbnails
+echo '' > /home/crowbar/.bash_history
+echo '' > /home/crowbar/.sqlite_history
+echo '' > /home/crowbar/.python_history
