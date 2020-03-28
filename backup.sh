d05d84f25e78f676ca715cca6b9b71de8c09eef2
Jon David Tannehill <jdtannehill2490@gmail.com>
2020-03-28 07:23:05 -0500

    modified backup.sh for my new setup. still untested on Parrot.

--- backup.sh
+++ backup.sh
@@ -1,36 +1,68 @@
 #!/bin/bash
 
 # "sudo systemctl start nfs-kernel-server"  or start nfs-server with startup.py on rock64 first!
+echo "Is nfs-kernel-server running on rock64? [y/n]"
+read REPLY
+if [[ "$REPLY" != [Yy] ]]; then
+	echo "You must first start nfs-kernel-server on your rock64!"
+	exit 2
+fi
 
 # make sure this is being run as root:
 if [ "$EUID" -ne 0 ]; then
 	echo This script must be run as root!
-	exit
+	exit 2
 fi
 
 # mount rock64
-rock64=$(
+ROCK64=$(
 	sudo arp-scan --localnet | \
 	egrep "2a:90:45:d6:eb:3e" | \
 	cut -f 1
 )
-rock64=$(
-	echo $rock64 | \
+ROCK64=$(
+	echo $ROCK64 | \
 	cut --delimiter=' ' -f 1
 )
-mount -t nfs $rock64: /mnt/nfs
+mount -t nfs $ROCK64: /mnt/nfs
 
-# mount your internal (windows) hard drive!
-read sd < <(ls /dev | egrep sd[abcd]4)
-hdd=/dev/$sd
-mount $hdd /mnt/hdd
+# mount other storage drives in the correct locations:
+ROOT_DEV=`cat /etc/mtab | grep "sd[ab][1-5] / " | cut -d " " -f1 | cut -d "/" -f3`
+if [[ "$ROOT_DEV" == sdb2 ]]; then
+	PARROT=/mnt/hdd2
+	sudo mount /dev/sda4 /mnt/hdd
+	sudo mount /dev/sdb4 $PARROT
+elif [[ "$ROOT_DEV" == sda2 ]]; then
+	PARROT=/mnt/hdd2
+	sudo mount /dev/sdb4 /mnt/hdd
+	sudo mount /dev/sda4 $PARROT
+elif [[ "$ROOT_DEV" == sdb4 ]]; then
+	DEBIAN=/mnt/hdd2
+	sudo mount /dev/sda4 /mnt/hdd
+	sudo mount /dev/sdb2 $DEBIAN
+elif [[ "$ROOT_DEV" == sda4 ]]; then
+	DEBIAN=/mnt/hdd2
+	sudo mount /dev/sdb4 /mnt/hdd
+	sudo mount /dev/sda2 $DEBIAN
+else
+	echo "Storage drives are not at their expected locations in /dev/.\nExiting now."
+	exit 2
+fi
 
 # now change into home directory and run rsync commands!
-cd /home/crowbar
-rsync -aAv --delete --exclude=.cache /home/crowbar /media/crowbar/Seagate\ Backup\ Plus\ Drive/backupDocs/rsync/debi
-rsync -aAv --delete /mnt/nfs /media/crowbar/Seagate\ Backup\ Plus\ Drive/backupDocs/rsync/rock64
-rsync -av --delete /mnt/hdd/Users/jdtan /media/crowbar/Seagate\ Backup\ Plus\ Drive/backupDocs/rsync/Windows
+cd $HOME
+sudo rsync -aAv --delete $HOME/Mutual /media/$USER/Seagate\ Backup\ Plus\ Drive/backupDocs/rsync
+sudo rsync -aAv --delete /mnt/nfs /media/$USER/Seagate\ Backup\ Plus\ Drive/backupDocs/rsync/rock64
+sudo rsync -av --delete /mnt/hdd/Users/jdtan /media/$USER/Seagate\ Backup\ Plus\ Drive/backupDocs/rsync/Windows
+if [[ "$USER" == "crowbar" ]]; then
+	sudo rsync -aAv --delete --exclude=.cache --exclude=Mutual $HOME /media/$USER/Seagate\ Backup\ Plus\ Drive/backupDocs/rsync/debi
+	sudo rsync -aAv --delete --exclude=.cache /mnt/hdd2/home/aves /media/$USER/Seagate\ Backup\ Plus\ Drive/backupDocs/rsync
+elif [[ "$USER" == "aves" ]]; then
+	sudo rsync -aAv --delete --exclude=.cache --exclude=Mutual $HOME /media/$USER/Seagate\ Backup\ Plus\ Drive/backupDocs/rsync
+	sudo rsync -aAv --delete --exclude=.cache /mnt/hdd2/home/crowbar /media/$USER/Seagate\ Backup\ Plus\ Drive/backupDocs/rsync/debi
+fi
 
-umount /mnt/nfs
-umount /mnt/hdd
+sudo umount /mnt/nfs
+sudo umount /mnt/hdd
+sudo umount /mnt/hdd2
 echo Done!
